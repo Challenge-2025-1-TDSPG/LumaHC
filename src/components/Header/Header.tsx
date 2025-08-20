de248d76d3c00fec00f0ce85e24bbf9c88c307de
KelsonZh0 <kel180498@gmail.com>
2025-08-19 22:27:34 -0300

    feat: Aplicando estilo na  imagem do usuario

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import MainMenu from './MainMenu';
-
+import user from '../../img/avatares/user.png';
 export default function Header() {
   return (
-    <header>
+    <header className='relative w-full h-auto bg-gradient' >
       <nav className='nav-bar'>
         <div className='profile'>
-          <img src='../../img/avatares/user.png' alt='Imagem de um Avatar' />
+          <img src={user} alt='Imagem de um Avatar' className='w-20 h-20 rounded-full object-cover shadow-[0_4px_12px_rgba(0,0,0,.5)] '/>
           <h2>Olá, Usuário!</h2>
           <p>Vamos te ajudar</p>
         </div>
